3ac68720e3725db940fcecb265cbb899b517eef8
MrBs <Chouaibwidadipur@gmail.com>
2023-09-12 02:16:35 +0100

    Fixed links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -13,17 +13,17 @@ export default function Socials() {
 	 rounded-md absolute sm:top-0 sm:right-0 text-white/50 bg-LightGrayishBlue items-center sm:justify-center justify-start"
     >
       <h1 className="uppercase tracking-widest text-sm ml-4">Share</h1>
-      <a href={"facebook.com"} className="w-5 h-5 rounded-full">
-        <img src={Facebook} className="" alt="Facebook" />
+      <a href={"#"} className="w-5 h-5 rounded-full">
+        <img src={Facebook} alt="Facebook" />
       </a>
-      <a href={"Twitter.com"} className="w-5 h-5 rounded-full">
+      <a href={"#"} className="w-5 h-5 rounded-full">
         <img
           src={Twitter}
           className="w-full h-full object-cover"
           alt="Twitter"
         />
       </a>
-      <a href={"Pinstrest.com"} className="w-5 h-5 rounded-full">
+      <a href={"#"} className="w-5 h-5 rounded-full">
         <img src={Pinstrest} className="" alt="Pinstrest" />
       </a>
       <div className="sm:block hidden triangle absolute bottom-0 left-1/2 -translate-x-1/3 translate-y-[95%]"></div>
